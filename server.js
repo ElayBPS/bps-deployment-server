947d20cff0f9febd1260aced79b160bd9e33a079
ElayBPS <wasavmada@gmail.com>
2019-04-18 11:59:30 +0100

    body-parser/server.use

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const server = express()
 
 const PORT = process.env.PORT || 80
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+    res.header("Access-Control-Expose-Headers", "Content-Type, Content-Length")
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
+
+server.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+server.use(bodyParser.json())
 
 server.post("/deploy", (req,res) => {
     const secret = req.query.secret
