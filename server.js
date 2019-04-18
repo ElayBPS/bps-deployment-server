db02dd4ece683e770ebe4079a2ae00bdf1937a12
ElayBPS <wasavmada@gmail.com>
2019-04-18 14:21:54 +0100

    async-await

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 
 const fs = require('fs')
 
+const exec = require('child-process-promise').exec
+
 const server = express()
 
 const PORT = process.env.PORT || 80
@@ -23,11 +25,15 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json())
 
-server.post("/deploy", (req,res) => {
+server.post("/deploy", async (req,res) => {
     const secret = req.query.secret
-    if (secret === "y2ocKkWgUM3pbisHLtbbwnDCoZddtT5D") {
+    if (secret === "y2ocKkWgUM3pbisHLtbbwnDCoZddtT5D" && req.body.ref === 'refs/heads/master') {
         console.log(req.body)
         fs.writeFileSync(__dirname+'/gitupdate-'+Date.now()+'.json', JSON.stringify(req.body, null, '\t'))
+        await exec('git clone '+ req.body.repository.clone_url)
+        await exec('gcloud builds submit --tag gcr.io/bps-test-ay/'+req.body.repository.name, {cwd:__dirname+'/'+req.body.repository.name})
+        await exec('gcloud beta run deploy --image gcr.io/bps-test-ay/'+req.body.repository.name, {cwd:__dirname+'/'+req.body.repository.name})
+        fs.rmdirSync(__dirname+'/'+req.body.repository.name)
         res.sendStatus(200)
     }
     else {
